efdfd96171b00d3f5ad945b1d879d09d9ceba077
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2024-04-20 13:04:05 +0530

    Adding employeemanagementapp  and projectServer in same repo

---
+++ employeemanagementapp/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Homepage from './components/Homepage';
+import EditPage from './components/EditPage';
+import AddPage from './components/AddPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome</h1>
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/add' element={<AddPage />} />
+        <Route path='/edit/:id' element={<EditPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
